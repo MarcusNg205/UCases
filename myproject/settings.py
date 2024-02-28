770081c1c02eef907f6513e8889c9da02fc31216
MarcusNg205 <161159606+MarcusNg205@users.noreply.github.com>
2024-02-28 16:01:28 -0500

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,11 +78,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
